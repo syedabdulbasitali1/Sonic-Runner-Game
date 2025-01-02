07cc088f5728e6e69a1e47ebb8569c7714e39a30
Abdul Basit Ali <syedabdulbasitali1@gmail..com>
2025-01-02 09:00:38 -0800

    game files uploaded

---
+++ src/script.js
@@ -0,0 +1,53 @@
+import k from "./kaplayCtx"
+import mainManu from "./scenes/mainManu";
+
+k.loadSprite("chemical-bg", "graphics/chemiacl-bg.png");
+k.loadSprite("platforms", "graphics/platforms.png");
+k.loadSprite("sonic", "graphics/sonic.png" , {
+    sliceX : 8,
+    sliceY : 2,
+    anims:{
+        run: {from: 0, to:7 , loop:true , speed:30},
+        jump: {from: 8, to:11, loop:true, speed:100},
+    }
+});
+k.loadSprite("ring" , "graphics/ring.png" ,{
+    sliceX: 16,
+    sliceY: 1,
+    anims: {
+        spin: {
+            from: 0,
+            to: 15,
+            loop: true,
+            speed: 30,
+        },
+    }
+});
+k.loadSprite("motobug" , "graphics/motobug.png", {
+    sliceX: 5,
+    sliceY: 1,
+    anims: {
+        from: 0,
+        to: 4,
+        loop: true,
+        speed: 8,
+    },
+});
+k.loadFont("mania" , "fonts/mania.ttf");
+k.loadSound("destroy" , "sounds/Destroy.wav");
+k.loadSound("hurt" , "sounds/Hurt.wav");
+k.loadSound("hyper-ring" , "sounds/HyperRing.wav");
+k.loadSound("ring" , "sounds/Ring.wav");
+k.loadSound("city" , "sounds/city.mp3");
+
+k.scene("main-manu" , mainManu);
+
+k.scene("game" , ()=>{
+
+});
+
+k.scene("game-over" , ()=>{
+
+});
+
+k.go("main-manu");
